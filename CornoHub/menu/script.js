442810c22fd8b425815202364230085480e04da4
Eric cauê <eric09feitosa@gmail.com>
2026-04-17 16:52:17 -0300

    Add files via upload

--- CornoHub/menu/script.js
+++ CornoHub/menu/script.js
@@ -1,6 +1,7 @@
 function login() {
     let email = document.getElementById("email").value;
     let senha = document.getElementById("senha").value;
+    let idade = Number(document.getElementById("idade").value)
 
     let emailvalido = email.includes("@")
     && email.includes(".com") && email.includes("gmail");
@@ -11,7 +12,10 @@ function login() {
     else if(!emailvalido) {
         alert("email invalido meu bem, tente novamente");
     }
+    else if(idade<18){
+        alert("você é de menor safado")
+    }
     else {
         window.location.href = "PaginaInicial/inicial.html";
-    }
+   } 
 }
